abbeef8248059f823bcc3b91590c27bcb59a6f33
Nikolay Koren <nikolai.koren2004@gmail.com>
2025-11-02 12:56:54 +0600

    feat: create controllers

---
+++ src/controllers/__init__.py
@@ -0,0 +1,5 @@
+from .article import ArticleController
+from .comment import CommentController
+from .user import UserController
+
+__all__ = ["ArticleController", "CommentController", "UserController"]
