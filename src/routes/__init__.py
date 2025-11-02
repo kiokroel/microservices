8f5acc5f43eff6f7343b8abc4d2ef59a706af089
Nikolay Koren <nikolai.koren2004@gmail.com>
2025-11-02 17:42:23 +0600

    fix: transfer routes

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,6 +2,8 @@ from .articles import router as articles_router
 from .users import router as users_router
 from .comments import router as comments_router
 
+from fastapi import APIRouter
+
 router = APIRouter()
 router.include_router(articles_router)
 router.include_router(users_router)
